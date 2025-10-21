508a909410c7fe269cec335cb8844fd6aeb87770
h <h@MacBook-Pro-h.local>
2025-10-21 18:51:57 +0900

    feat: publish propertylist page

---
+++ libs/config.ts
@@ -0,0 +1,7 @@
+export const propertyTypes: string[] = ["APARTMENT", "HOUSE", "VILLA"];
+
+export const cities: string[] = ["SEOUL", "BUSAN", "INCHEON", "DAEGU"];
+
+export const propertySquare: number[] = [
+  0, 25, 50, 75, 100, 125, 150, 200, 300, 500,
+];
